2ecc6e35727703e8973671d8b9fd738d654c1fbe
r-d-jorgensen <r.d.jorgensen@protonmail.com>
2025-01-02 21:50:48 -0600

    update sql table and add output for manual debit data add

--- finance_db_schema.sql
+++ finance_db_schema.sql
@@ -3,8 +3,8 @@ CREATE TABLE IF NOT EXISTS books (
     name TEXT NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS bank_accounts (
-    bank_account_id INTEGER PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS accounts (
+    account_id INTEGER PRIMARY KEY,
     book_id INTEGER NOT NULL,
     name TEXT NOT NULL,
     FOREIGN KEY (book_id) REFERENCES books (book_id) ON DELETE CASCADE
@@ -19,13 +19,13 @@ CREATE TABLE IF NOT EXISTS categories (
 
 CREATE TABLE IF NOT EXISTS records (
     record_id INTEGER PRIMARY KEY,
-    bank_account_id INTEGER NOT NULL,
+    account_id INTEGER NOT NULL,
     category_id INTEGER NOT NULL,
     amount INTEGER NOT NULL,
     business TEXT NOT NULL,
     location TEXT,
     note TEXT,
     transaction_date DATETIME NOT NULL,
-    FOREIGN KEY (bank_account_id) REFERENCES bank_accounts (bank_account_id) ON DELETE CASCADE
+    FOREIGN KEY (account_id) REFERENCES accounts (account_id) ON DELETE CASCADE
     FOREIGN KEY (category_id) REFERENCES categories (category_id) ON DELETE CASCADE
 );
--- pdf_transaction_scraper.py
+++ pdf_transaction_scraper.py
@@ -14,12 +14,14 @@ class NoData(Exception):
 
 def parse_navy_federal_pdf(pdf_file:str)->tuple:
     """Parses all transaction info from pdf"""
-    transactions = ([], [])
+    transactions_debits, transactions_credits = [], []
     reader = PdfReader(pdf_file)
     debits_found, credits_found = False, False
 
     start_of_debits = "Trans Date Post Date Reference No. Description Submitted By Amount"
     end_of_debits = "TOTAL PAYMENTS AND CREDITS"
+    mobile_payment = "MOB PAYMENT RECEIVED"
+    online_payment = "NFO PAYMENT RECEIVED"
 
     start_of_credits = "Trans Date Post Date Reference No."
     end_of_credits = f"TOTAL New Activity for {os.getenv("LEGAL_NAME")}"
@@ -29,15 +31,16 @@ def parse_navy_federal_pdf(pdf_file:str)->tuple:
             if end_of_debits in line:
                 debits_found = False
             elif end_of_credits in line:
-                return transactions
+                return transactions_debits, transactions_credits
 
             if start_of_debits in line:
                 debits_found = True
             elif start_of_credits in line:
                 credits_found = True
             elif debits_found:
-                # TODO: debits should be parsed here
-                transactions[0].append(line)
+                if mobile_payment in line or online_payment in line:
+                    continue
+                transactions_debits.append(line)
             elif credits_found:
                 if "TRANSACTIONS" in line:
                     continue
@@ -47,7 +50,7 @@ def parse_navy_federal_pdf(pdf_file:str)->tuple:
                     continue
 
                 data_point = parse_transaction_line(line)
-                transactions[1].append(data_point)
+                transactions_credits.append(data_point)
     raise NoData()
 
 def parse_transaction_line(data_line:str)->list:
@@ -72,7 +75,7 @@ def parse_transaction_line(data_line:str)->list:
 def get_category(book_id:int, amount:float, business:str, location:str, trasaction_date:datetime)->int:
     """Gets the category of the transaction"""
     try:
-        sql_statment = "SELECT category_id FROM records WHERE bank_account_id = ? AND business LIKE ?;"
+        sql_statment = "SELECT category_id FROM records WHERE account_id = ? AND business LIKE ?;"
         category_id = sql_get(sql_statment, [book_id, business])[0][0]
     except IndexError:
         sql_statment = "SELECT * FROM categories WHERE book_id = ?;"
@@ -132,18 +135,27 @@ def sql_insert(sql_statment:str, sql_paramiters:list)->int:
 
 def load_pdf_data(book_id:int, bank_account_id:int, pdf_file:str)->None:
     """Driver function of script"""
-    sql_check_statment = "SELECT * FROM records WHERE bank_account_id = ? AND category_id = ? AND \
+    sql_check_statment = "SELECT * FROM records WHERE account_id = ? AND category_id = ? AND \
         amount = ? AND business = ? AND location = ? AND note = ? AND transaction_date = ?;"
     sql_insert_statment = "INSERT INTO records \
             (bank_account_id, category_id, amount, business, location, note, transaction_date) \
             VALUES (?, ?, ?, ?, ?, ?, ?);"
 
-    _, transaction_credits = parse_navy_federal_pdf(pdf_file)
+    transaction_debits, transaction_credits = parse_navy_federal_pdf(pdf_file)
     for record in transaction_credits:
         record[0] = bank_account_id
         record[1] = get_category(book_id, record[2], record[3], record[4], record[6])
         if len(sql_get(sql_check_statment, record)) == 0:
             sql_insert(sql_insert_statment, record)
+
+    if transaction_debits:
+        print("----------DEBITS ON ACCOUNT---------------")
+        for record in transaction_debits:
+            print(record)
+        print("------------------------------------------")
+        print("INSERT INTO records (bank_account_id, category_id, amount, business, location, note, transaction_date)")
+        print("VALUES (1, 0, 0.00, '', ', '', '');")
+        print("------------------------------------------")
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
