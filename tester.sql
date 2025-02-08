21d7cce1b88456c257ec7c4209426a4901e11750
r-d-jorgensen <r.d.jorgensen@protonmail.com>
2025-02-07 20:32:26 -0600

    update to match schema

--- tester.sql
+++ tester.sql
@@ -1,3 +1,3 @@
 INSERT INTO books(name) VALUES('Tester');
-INSERT INTO bank_accounts(book_id, name) VALUES(1, 'Test Account');
+INSERT INTO accounts(book_id, name) VALUES(1, 'Test Account');
 INSERT INTO categories(book_id, name) VALUES(1, 'General');
